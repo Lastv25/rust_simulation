3fbd9ac8d2ffbf077d68d95cca5141dad2b5d6a8
Lastv25 <hg.mnjx@gmail.com>
2023-08-26 11:09:55 +0200

    cargo start project build

---
+++ project/simulation/src/main.rs
@@ -0,0 +1,24 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Simulation Fun".into(),
+                        resolution: (640.0, 480.0).into(),
+                        resizable: false,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .build(),
+        )
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
